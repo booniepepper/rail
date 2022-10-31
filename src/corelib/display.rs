bc1ff7e6ccc7fd249a4903e538cea62c934753c4
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-10-31 02:26:58 -0700

    More custom stdout printing, better pipe handling

--- src/corelib/display.rs
+++ src/corelib/display.rs
@@ -1,15 +1,22 @@
 use std::fmt::Display;
+use std::io::{self, Stdout, Write};
 
 use crate::rail_machine::{RailDef, RailType, RailVal};
 use crate::RAIL_VERSION;
 
 pub fn builtins() -> Vec<RailDef<'static>> {
     vec![
-        printer("p", "FIXME", &|a| print!("{}", a)),
-        printer("pl", "FIXME", &|a| println!("{}", a)),
-        RailDef::contextless("nl", "FIXME", &[], &[], || print!("\n")),
+        printer("p", "FIXME", &|a| {
+            print_or_die(|out| out.write_fmt(format_args!("{}", a)))
+        }),
+        printer("pl", "FIXME", &|a| {
+            print_or_die(|out| out.write_fmt(format_args!("{}\n", a)))
+        }),
+        RailDef::contextless("nl", "FIXME", &[], &[], || {
+            print_or_die(|out| out.write(b"\n"))
+        }),
         RailDef::on_state("status", "FIXME", &[], &[], |state| {
-            println!("{}", state.stack);
+            print_or_die(|out| out.write_fmt(format_args!("{}\n", state.stack)));
             state
         }),
         RailDef::contextless("clear", "FIXME", &[], &[], || {
@@ -33,4 +40,14 @@ where
         }
         quote
     })
+}
+
+fn print_or_die<P, T>(p: P)
+where
+    P: Fn(&mut Stdout) -> io::Result<T>,
+{
+    let res = p(&mut io::stdout());
+    if res.is_err() {
+        std::process::exit(1);
+    }
 }
