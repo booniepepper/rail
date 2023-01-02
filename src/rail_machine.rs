07281996f938af725555d5fda37c2371540893f3
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-01-02 03:05:26 -0800

    Refactor token runner in rail_machine

--- src/rail_machine.rs
+++ src/rail_machine.rs
@@ -74,22 +74,20 @@ impl RailState {
             Token::I64(i) => self.push_i64(i),
             Token::F64(f) => self.push_f64(f),
             Token::Term(term) => {
-                if let Some(op) = self.clone().get_def(&term) {
-                    if self.in_main() {
+                match (self.clone().get_def(&term), self.in_main()) {
+                    (Some(op), true) => {
                         let mut op = op;
                         op.act(self)
-                    } else {
-                        self.push_command(&op.name)
                     }
-                } else if !self.in_main() {
-                    // We optimistically expect this may be a not-yet-defined term. This
-                    // gives a way to do recursive definitions.
-                    self.push_command(&term)
-                } else {
-                    // TODO: Use a logging library? Log levels? Exit in a strict mode?
-                    // TODO: Have/get details on filename/source, line number, character number
-                    let term = term.replace('\n', "\\n");
-                    derail_for_unknown_command(&term, self.conventions);
+                    (Some(op), false) => self.push_command(&op.name),
+                    (None, false) => self.push_command(&term),
+                    (None, true) => {
+                        // Unknown term executing in main.
+                        // TODO: Use a logging library? Log levels? Exit in a strict mode?
+                        // TODO: Have/get details on filename/source, line number, character number
+                        let term = term.replace('\n', "\\n");
+                        derail_for_unknown_command(&term, self.conventions);
+                    }
                 }
             }
         }
