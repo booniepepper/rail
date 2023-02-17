c867d9adab22c71ba943dd57444fb8f1d30b4a7e
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-02-17 02:11:26 -0800

    Process REPL line-by-line

--- src/prompt.rs
+++ src/prompt.rs
@@ -8,7 +8,6 @@ use rustyline::Editor;
 pub struct RailPrompt {
     is_tty: bool,
     editor: Editor<()>,
-    terms: Vec<Token>,
     conventions: &'static RunConventions<'static>,
 }
 
@@ -16,11 +15,10 @@ impl RailPrompt {
     pub fn new(conventions: &'static RunConventions) -> RailPrompt {
         let mut editor = Editor::<()>::new().expect("Unable to boot editor");
         let is_tty = editor.dimensions().is_some();
-        let terms = vec![];
+
         RailPrompt {
             is_tty,
             editor,
-            terms,
             conventions,
         }
     }
@@ -32,7 +30,7 @@ impl RailPrompt {
         );
         eprintln!("{}", name_and_version.dimmed().red());
 
-        let end_state = self.fold(state, |state, term| state.run_token(term));
+        let end_state = self.fold(state, |state, term| state.run_tokens(term));
 
         if !end_state.stack.is_empty() {
             let end_state_msg = format!("State dump: {}", end_state.stack);
@@ -42,41 +40,36 @@ impl RailPrompt {
 }
 
 impl Iterator for RailPrompt {
-    type Item = Token;
+    type Item = Vec<Token>;
 
-    fn next(&mut self) -> Option<Token> {
-        while self.terms.is_empty() {
-            // If we're interactive with a human (at a TTY and not piped stdin),
-            // we pad with a newline in case the user uses print without newline.
-            // (Otherwise, the prompt will rewrite the line with output.)
-            if self.is_tty {
-                println!();
-            }
-
-            let input = self.editor.readline("> ");
-
-            if let Err(e) = input {
-                // ^D and ^C are not error cases.
-                if let ReadlineError::Eof = e {
-                    rail_machine::log_fatal(self.conventions, "End of input");
-                    return None;
-                } else if let ReadlineError::Interrupted = e {
-                    rail_machine::log_fatal(self.conventions, "Process interrupt");
-                    return None;
-                }
-
-                rail_machine::log_fatal(self.conventions, e);
-                std::process::exit(1);
-            }
-
-            let input = input.unwrap();
-
-            self.editor.add_history_entry(&input);
-
-            self.terms = loading::get_source_as_tokens(input);
-            self.terms.reverse();
+    fn next(&mut self) -> Option<Self::Item> {
+        // If we're interactive with a human (at a TTY and not piped stdin),
+        // we pad with a newline in case the user uses print without newline.
+        // (Otherwise, the prompt will rewrite the line with output.)
+        if self.is_tty {
+            println!();
         }
 
-        self.terms.pop()
+        let input = self.editor.readline("> ");
+
+        if let Err(e) = input {
+            // ^D and ^C are not error cases.
+            if let ReadlineError::Eof = e {
+                rail_machine::log_fatal(self.conventions, "End of input");
+                return None;
+            } else if let ReadlineError::Interrupted = e {
+                rail_machine::log_fatal(self.conventions, "Process interrupt");
+                return None;
+            }
+
+            rail_machine::log_fatal(self.conventions, e);
+            std::process::exit(1);
+        }
+
+        let input = input.unwrap();
+
+        self.editor.add_history_entry(&input);
+
+        Some(loading::get_source_as_tokens(input))
     }
 }
