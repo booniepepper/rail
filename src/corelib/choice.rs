f02e89c7861d8d5ade88c1ad207a015b04f49b84
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-04-12 01:45:29 -0700

    Rename opt to ?, do not modify stack for conditions in ?

--- src/corelib/choice.rs
+++ src/corelib/choice.rs
@@ -3,7 +3,7 @@ use crate::rail_machine::{RailDef, RailType};
 pub fn builtins() -> Vec<RailDef<'static>> {
     // TODO: Redesign. Is this a symbol table?
     vec![RailDef::on_state(
-        "opt",
+        "?",
         "Consumes a specially-formed quote of quotes. The quote of quotes is
         evaluated by twos, with the first quote as a predicate and the second
         as an action to perform. Predicates will be evaluated until a predicate
@@ -12,20 +12,19 @@ pub fn builtins() -> Vec<RailDef<'static>> {
         &[RailType::Quote],
         &[],
         |state| {
-            // TODO: All conditions and all actions must have the same quote effect.
-            let (options, quote) = state.stack.clone().pop_quote("opt");
-            let mut state = state.replace_stack(quote);
+            // TODO: All conditions and all actions must have the same stack effect.
+            let (options, quote) = state.stack.clone().pop_quote("?");
+            let state = state.replace_stack(quote);
 
             let mut options = options.reverse();
 
             while !options.is_empty() {
-                let (condition, opts) = options.pop_quote("opt");
-                let (action, opts) = opts.pop_quote("opt");
+                let (condition, opts) = options.pop_quote("?");
+                let (action, opts) = opts.pop_quote("?");
                 options = opts;
 
-                state = condition.jailed_run_in_state(state)?;
-                let (success, quote) = state.stack.clone().pop_bool("opt");
-                state = state.replace_stack(quote);
+                let cond_state = condition.jailed_run_in_state(state.clone())?;
+                let (success, _) = cond_state.stack.clone().pop_bool("?");
 
                 if success {
                     return action.run_in_state(state);
